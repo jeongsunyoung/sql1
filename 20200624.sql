fff86c99d9937235be59d33e20897c37e560a49b
sunyoung <tnghtjsdud@gmail.com>
2020-06-25 10:14:18 +0900

    20200624

---
+++ 20200624.sql
@@ -0,0 +1,33 @@
+사용자 생성 및 테이블 생성
+userCraate.sql
+scott.sql
+base_table.sql
+users.sql
+
+
+SELECT 실습
+문법
+SELECT * | [colum1, colum2, ....]
+FROM 테이블명;
+
+SELECT * 
+FROM prod;
+
+SELECT prod_id, prod_name
+FROM prod;
+
+
+SELECT (실습 select1)
+
+SELECT *
+FROM lprod;
+
+SELECT buyer_id, buyer_name
+FROM buyer;
+
+SELECT *
+FROM cart;
+
+SELECT mem_id, mem_pass, mem_name
+FROM member;
+
